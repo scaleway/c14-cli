923af81ad22f768dfb8ed9ae0c2d3260c8add51b
Quentin Perez <qperez@ocs.online.net>
2016-06-22 16:49:39 +0200

    helpers -> methods

--- pkg/api/helpers.go
+++ pkg/api/helpers.go
@@ -1,149 +1,53 @@
 package api
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/juju/errors"
 )
 
-/*
- * Get Functions
- */
-
-// GetSafes returns a list of safe
-func (o *OnlineAPI) GetSafes() (safes []OnlineGetSafe, err error) {
-	if err = o.getWrapper(fmt.Sprintf("%s/storage/c14/safe", APIUrl), &safes); err != nil {
-		err = errors.Annotate(err, "GetSafes")
-	}
-	return
+type ConfigCreateSSHBucketFromScratch struct {
+	SafeName    string
+	ArchiveName string
+	Desc        string
+	UUIDSSHKeys []string
+	Platforms   []string
 }
 
-// GetSafe returns a safe
-func (o *OnlineAPI) GetSafe(uuid string) (safe OnlineGetSafe, err error) {
-	// TODO: enable to use the name instead of only the UUID
-	if err = o.getWrapper(fmt.Sprintf("%s/storage/c14/safe/%s", APIUrl, uuid), &safe); err != nil {
-		err = errors.Annotate(err, "GetSafe")
-	}
-	return
-}
-
-// GetPlatforms returns a list of platform
-func (o *OnlineAPI) GetPlatforms() (platform []OnlineGetPlatform, err error) {
-	if err = o.getWrapper(fmt.Sprintf("%s/storage/c14/platform", APIUrl), &platform); err != nil {
-		err = errors.Annotate(err, "GetPlatforms")
-	}
-	return
-}
-
-// GetPlatform returns a platform
-func (o *OnlineAPI) GetPlatform(uuid string) (platform OnlineGetPlatform, err error) {
-	// TODO: enable to use the name instead of only the UUID
-	if err = o.getWrapper(fmt.Sprintf("%s/storage/c14/platform/%s", APIUrl, uuid), &platform); err != nil {
-		err = errors.Annotate(err, "GetPlatform")
-	}
-	return
-}
-
-func (o *OnlineAPI) GetSSHKeys() (keys []OnlineGetSSHKey, err error) {
-	if err = o.getWrapper(fmt.Sprintf("%s/user/key/ssh", APIUrl), &keys); err != nil {
-		err = errors.Annotate(err, "GetSSHKeys")
-	}
-	return
-}
-
-func (o *OnlineAPI) GetSSHKey(uuid string) (key OnlineGetSSHKey, err error) {
-	if err = o.getWrapper(fmt.Sprintf("%s/user/key/ssh/%s", APIUrl, uuid), &key); err != nil {
-		err = errors.Annotate(err, "GetSSHKey")
-	}
-	return
-}
-
-func (o *OnlineAPI) GetArchives(uuidSafe string) (archives []OnlineGetArchive, err error) {
-	if err = o.getWrapper(fmt.Sprintf("%s/storage/c14/safe/%s/archive", APIUrl, uuidSafe), &archives); err != nil {
-		err = errors.Annotate(err, "GetArchives")
-	}
-	return
-}
-
-func (o *OnlineAPI) GetArchive(uuidSafe, uuidArchive string) (archive OnlineGetArchive, err error) {
-	if err = o.getWrapper(fmt.Sprintf("%s/storage/c14/safe/%s/archive/%s", APIUrl, uuidSafe, uuidArchive), &archive); err != nil {
-		err = errors.Annotate(err, "GetArchive")
-	}
-	return
-}
-
-func (o *OnlineAPI) GetBucket(uuidSafe, uuidArchive string) (bucket OnlineGetBucket, err error) {
-	if err = o.getWrapper(fmt.Sprintf("%s/storage/c14/safe/%s/archive/%s/bucket", APIUrl, uuidSafe, uuidArchive), &bucket); err != nil {
-		err = errors.Annotate(err, "GetBucket")
-	}
-	return
-}
-
-/*
- * Create Functions
- */
-
-func (o *OnlineAPI) CreateSafe(name, desc string) (uuid string, err error) {
-	var (
-		buff []byte
-	)
-
-	if buff, err = o.postWrapper(fmt.Sprintf("%s/storage/c14/safe", APIUrl), OnlinePostSafe{
-		Name:        name,
-		Description: desc,
-	}); err != nil {
-		err = errors.Annotate(err, "CreateSafe")
+// CreateSSHBucketFromScratch creates a safe, an archive and returns the bucket available over SSH
+func (o *OnlineAPI) CreateSSHBucketFromScratch(c ConfigCreateSSHBucketFromScratch) (uuidSafe, uuidArchive string, bucket OnlineGetBucket, err error) {
+	if uuidSafe, err = o.CreateSafe(c.SafeName, ""); err != nil {
+		err = errors.Annotate(err, "CreateSSHBucketFromScratch:CreateSafe")
 		return
 	}
-	uuid = string(buff)
-	return
-}
-
-type ConfigCreateArchive struct {
-	UUIDSafe  string
-	Name      string
-	Desc      string
-	Parity    string
-	Protocols []string
-	SSHKeys   []string
-	Platforms []string
-	Days      int
-}
-
-func (o *OnlineAPI) CreateArchive(config ConfigCreateArchive) (uuid string, err error) {
-	var (
-		buff []byte
-	)
-
-	if buff, err = o.postWrapper(fmt.Sprintf("%s/storage/c14/safe/%s/archive", APIUrl, config.UUIDSafe), OnlinePostArchive{
-		Name:        config.Name,
-		Description: config.Desc,
-		Protocols:   config.Protocols,
-		SSHKeys:     config.SSHKeys,
-		Platforms:   config.Platforms,
-		Days:        config.Days,
+	if uuidArchive, err = o.CreateArchive(ConfigCreateArchive{
+		UUIDSafe:  uuidSafe,
+		Name:      c.ArchiveName,
+		Desc:      c.Desc,
+		Protocols: []string{"SSH"},
+		Platforms: c.Platforms,
+		SSHKeys:   c.UUIDSSHKeys,
+		Days:      7,
 	}); err != nil {
-		err = errors.Annotate(err, "CreateArchive")
+		o.DeleteSafe(uuidSafe)
+		err = errors.Annotate(err, "CreateSSHBucketFromScratch:CreateArchive")
 		return
 	}
-	fmt.Println(string(buff))
-	return
-}
-
-/*
- * Delete Functions
- */
-
-func (o *OnlineAPI) DeleteSafe(uuid string) (err error) {
-	if err = o.deleteWrapper(fmt.Sprintf("%s/storage/c14/safe/%s", APIUrl, uuid)); err != nil {
-		err = errors.Annotate(err, "DeleteSafe")
+	for i := 0; i < 60; i++ {
+		err = nil
+		if bucket, err = o.GetBucket(uuidSafe, uuidArchive); err == nil {
+			break
+		}
+		if onlineError, ok := errors.Cause(err).(*OnlineError); ok && onlineError.StatusCode != 404 {
+			return
+		}
+		time.Sleep(1 * time.Second)
 	}
-	return
-}
-
-func (o *OnlineAPI) DeleteArchive(uuidSafe, uuidArchive string) (err error) {
-	if err = o.deleteWrapper(fmt.Sprintf("%s/storage/c14/safe/%s/archive/%s", APIUrl, uuidSafe, uuidArchive)); err != nil {
-		err = errors.Annotate(err, "DeleteArchive")
+	if err != nil {
+		o.DeleteArchive(uuidSafe, uuidArchive)
+		o.DeleteSafe(uuidSafe)
+		err = errors.Annotate(err, "CreateSSHBucketFromScratch:GetBucket")
+		return
 	}
 	return
 }
