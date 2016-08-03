0646abd8087e81d32c6327ad0d6be4375371dd7c
Quentin Perez <qperez@ocs.online.net>
2016-08-03 14:19:04 +0200

    version: alpha v0.1

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION represents the version of c14
-	VERSION = "v1.0"
+	VERSION = "v0.1"
 	// GITCOMMIT is overlaoded by the Makefile
 	GITCOMMIT = "commit"
 	// UserAgent represents the user-agent used for the API calls
