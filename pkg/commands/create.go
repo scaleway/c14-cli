901babdac8c2f59e0ecb8e01dc32d3339526197f
Quentin Selle <qselle@online.net>
2018-03-15 21:41:42 +0100

    change platforms option to zone

--- pkg/commands/create.go
+++ pkg/commands/create.go
@@ -17,15 +17,15 @@ type create struct {
 }
 
 type createFlags struct {
-	flName     string
-	flDesc     string
-	flSafe     string
-	flQuiet    bool
-	flParity   string
-	flLarge    bool
-	flCrypto   bool
-	flSshKeys  string
-	flPlatform string
+	flName    string
+	flDesc    string
+	flSafe    string
+	flQuiet   bool
+	flParity  string
+	flLarge   bool
+	flCrypto  bool
+	flSshKeys string
+	flZone    string
 }
 
 // Create returns a new command "create"
@@ -50,7 +50,7 @@ func Create() Command {
 	ret.Flags.BoolVar(&ret.flLarge, []string{"l", "-large"}, false, "Ask for a large bucket")
 	ret.Flags.BoolVar(&ret.flCrypto, []string{"c", "-crypto"}, true, "Enable aes-256-bc cryptography, enabled by default.")
 	ret.Flags.StringVar(&ret.flSshKeys, []string{"k", "-sshkey"}, "", "A list of UUIDs corresponding to the SSH keys (separated by a comma) that will be used for the connection.")
-	ret.Flags.StringVar(&ret.flPlatform, []string{"z", "-zone"}, "2", "Show the zone (platforms) ")
+	ret.Flags.StringVar(&ret.flZone, []string{"z", "-zone"}, "2", "Show the zone (platforms) ")
 	return ret
 }
 
@@ -123,7 +123,7 @@ func (c *create) Run(args []string) (err error) {
 		ArchiveName: c.flName,
 		Desc:        c.flDesc,
 		UUIDSSHKeys: UuidSshKeys,
-		Platforms:   []string{c.flPlatform},
+		Platforms:   []string{c.flZone},
 		Days:        7,
 		Quiet:       c.flQuiet,
 		Parity:      c.flParity,
