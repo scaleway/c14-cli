8d94c91bbdbe7d5dd8887a7b3e0d1c85b49acbf1
Vincent Auclair <vauclair@online.net>
2018-04-05 16:00:07 +0200

    version: dc4 v0.3

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION represents the version of c14
-	VERSION = "v0.2"
+	VERSION = "v0.3"
 	// GITCOMMIT is overlaoded by the Makefile
 	GITCOMMIT = "commit"
 	// UserAgent represents the user-agent used for the API calls
