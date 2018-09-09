bca9c942380878c0efa0778203a162bf34be7934
SegFault42 <SegFault42@protonmail.com>
2018-09-09 17:16:48 +0200

    update version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION represents the version of c14
-	VERSION = "v0.3"
+	VERSION = "v0.4"
 	// GITCOMMIT is overlaoded by the Makefile
 	GITCOMMIT = "commit"
 	// UserAgent represents the user-agent used for the API calls
