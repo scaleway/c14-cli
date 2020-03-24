ee65dbcde9403b65b7390b4bf0efd8260899e7ba
thibault ketterer <thekit@gmail.com>
2020-03-24 15:19:54 +0100

    feat: list unix timestamp of archive to permit easy old archive deletion (#63)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION represents the version of c14
-	VERSION = "v0.4"
+	VERSION = "v0.4.1"
 	// GITCOMMIT is overlaoded by the Makefile
 	GITCOMMIT = "commit"
 	// UserAgent represents the user-agent used for the API calls
