766f11785cf379cbd5357aec8661c4e330ab2ac3
Stanislas Lange <angristan@pm.me>
2020-05-26 15:36:21 +0200

    Bump version to v0.5.0
    Signed-off-by: Stanislas Lange <angristan@pm.me>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// VERSION represents the version of c14
-	VERSION = "v0.4.1"
+	VERSION = "v0.5.0"
 	// GITCOMMIT is overlaoded by the Makefile
 	GITCOMMIT = "commit"
 	// UserAgent represents the user-agent used for the API calls
